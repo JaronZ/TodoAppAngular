de26a2f164a7ffc0240c43e8e63cbdc084d6d5b9
Jaron Zijlstra <support@jaronline.dev>
2025-10-28 17:13:00 +0100

    feat: add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {Todo} from './todo/todo';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: Todo,
+    title: "Todo List"
+  }
+];
